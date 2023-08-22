55a99c59b0efd79562ac93a67f878a3d8a436a9e
Efe Ozalp <efeozalp0101@gmail.com>
2023-08-22 10:58:19 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+            Image("car")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
