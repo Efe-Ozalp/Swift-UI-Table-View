a98cc8f3299ddd9e8e901158158029888c08d23d
Efe Ozalp <efeozalp0101@gmail.com>
2023-08-28 10:26:57 -0500

    I made a new file with table view

---
+++ TableView.swift
@@ -0,0 +1,20 @@
+//
+//  TableView.swift
+//  Swift UI Table View
+//
+//  Created by Efe Ozalp on 8/28/23.
+//
+
+import SwiftUI
+
+struct TableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TableView_Previews: PreviewProvider {
+    static var previews: some View {
+        TableView()
+    }
+}
