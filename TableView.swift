f84b171343c255600b33a5dfc4f1340797518772
Efe Ozalp <efeozalp0101@gmail.com>
2023-08-28 11:13:15 -0500

    I made a new burger image in assets, added a list item, and called TableView in ContentView so the app could actually show it !!! We getting there....

--- TableView.swift
+++ TableView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 struct TableView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       ListRow()
     }
 }
+
+struct ListRow: View {
+    var body: some View {
+        HStack{
+            Text("Food")
+            Spacer()
+            Image("burger")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: 40)
+        }
+    }
+}
+
 struct Food: Identifiable {
     var id: Int
     var name: String
@@ -19,7 +33,10 @@ struct Food: Identifiable {
 
 var myFoods = [
     Food(id: 1, name: "Hamburger"),
-    Food(id: 2, name: "Pizza")
+    Food(id: 2, name: "Pizza"),
+    Food(id: 3, name: "Ramen"),
+    Food(id: 4, name: "Sandwich"),
+    Food(id: 5, name: "Taco")
 ]
 struct TableView_Previews: PreviewProvider {
     static var previews: some View {
