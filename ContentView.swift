6c07df551ecf3d28fee20f098f721457e06fe266
Efe Ozalp <efeozalp0101@gmail.com>
2023-08-28 10:37:34 -0500

    I think we should make it a food list kind of thing I made some changes to table view

--- ContentView.swift
+++ ContentView.swift
@@ -4,9 +4,6 @@ struct ContentView: View {
     var body: some View {
         VStack {
 
-            Image("car")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, everyone!")
             
             
--- TableView.swift
+++ TableView.swift
@@ -12,7 +12,15 @@ struct TableView: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
+struct Food: Identifiable {
+    var id: Int
+    var name: String
+}
 
+var myFoods = [
+    Food(id: 1, name: "Hamburger"),
+    Food(id: 2, name: "Pizza")
+]
 struct TableView_Previews: PreviewProvider {
     static var previews: some View {
         TableView()
